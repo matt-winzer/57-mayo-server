4a1db54597750bb3dc1dce2e461d55112384bf78
Matt Winzer <matt.winzer@gmail.com>
2018-11-26 14:40:34 -0700

    completed #1: set up basic server

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 require('dotenv').config()
 const express = require('express')
-const port = process.env.PORT || 3000
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
+const cors = require('cors')
 
+const port = process.env.PORT || 3000
 const app = express()
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(morgan(process.env.NODE_ENV !== 'production' ? 'dev' : 'combined'))
+app.use(cors())
 
 app.get('/', (req, res) => {
   res.json({ message: 'Server working!'})
 })
 
 
-app.listen(port, () => console.log(`Listening on port: ${port}`))
+app.listen(port)
+  .on('listening', () => console.log(`Listening on port: ${port}`))
