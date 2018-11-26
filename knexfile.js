ad31cd32a251b51019f080571e3e13aa784dd4c1
Matt Winzer <matt.winzer@gmail.com>
2018-11-26 15:04:55 -0700

    closed #3: knex initialized and configured

--- knexfile.js
+++ knexfile.js
@@ -3,42 +3,13 @@
 module.exports = {
 
   development: {
-    client: 'sqlite3',
-    connection: {
-      filename: './dev.sqlite3'
-    }
+    client: 'pg',
+    connection: 'postgres://localhost/57-mayo'
   },
 
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
+  production:{
+    client: 'pg',
+    connection: process.env.DATABASE_URL + '?ssl=true'
   }
 
-};
+}
