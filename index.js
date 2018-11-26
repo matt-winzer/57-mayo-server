788528e6fb8086fe55e72d0e09fcaf32a62b1845
Matt Winzer <matt.winzer@gmail.com>
2018-11-26 13:57:01 -0700

    added '/' route indicating working server

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
+require('dotenv').config()
 const express = require('express')
 const port = process.env.PORT || 3000
 
 const app = express()
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Server working!'})
+})
 
 
 app.listen(port, () => console.log(`Listening on port: ${port}`))
