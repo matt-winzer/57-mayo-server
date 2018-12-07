511d5735ed68e1c9e9a72ef651f3537cd1017b35
Matt Winzer <matt-winzer@users.noreply.github.com>
2018-12-07 13:26:08 -0700

    Token expiration (#13)
    * added 1d expiration on login/signup; exp checked in users routes
    
    * small refactor

--- routes/users.js
+++ routes/users.js
@@ -3,31 +3,39 @@ const router = express.Router()
 
 const knex = require('../db/connection')
 const jwt = require('jsonwebtoken')
+const util = require('util')
+const verifyToken = util.promisify(jwt.verify)
 
 router.get('/:id/reviews', (req, res, next) => {
   if (!req.headers.authorization) res.status(401).json({ error: 'Unauthorized. Please login or signup for an account.' })
   else {
     const id = req.params.id
     const token = req.headers.authorization.slice(7)
-    const decoded = jwt.verify(token, process.env.TOKEN_SECRET)
-    
-    if (decoded.id == id) {
-      knex('purchase')
-        .select(
-          'review.id',
-          'review.created_at',
-          'review.updated_at',
-          'review.market',
-          'review.state',
-          'review.content',
-        )
-        .innerJoin('review', 'purchase.review_id', 'review.id')
-        .where('purchase.users_id', id)
-        .then(reviews => {
-          res.json({ reviews })
-        })
-    }
-    else res.status(401).json({ error: 'Unauthorized. Please login or signup for an account'})
+
+    verifyToken(token, process.env.TOKEN_SECRET)
+      .then(decoded => {
+        if (decoded.id == id) {
+          return knex('purchase')
+            .select(
+              'review.id',
+              'review.created_at',
+              'review.updated_at',
+              'review.market',
+              'review.state',
+              'review.content',
+            )
+            .innerJoin('review', 'purchase.review_id', 'review.id')
+            .where('purchase.users_id', id)
+            .then(reviews => {
+              res.json({ reviews })
+            })
+        }
+        else res.status(401).json({ error: 'Unauthorized. Please login or signup for an account' })
+      })
+      .catch(error => {
+        if (error.name === 'TokenExpiredError') res.status(401).json({ error: 'Token expired. Please login again.' })
+        else next(error)
+      })
   }
 })
 
